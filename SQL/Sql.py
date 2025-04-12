69b12d0a2f4c4bbc5a483997dfad256f4730d4c8
Sartc00l <sukindima025@gmail.com>
2025-04-12 13:24:32 +0300

    add sql

--- SQL/Sql.py
+++ SQL/Sql.py
@@ -2,6 +2,17 @@ import tkinter as tk
 from tkinter import messagebox
 import mysql.connector
 from mysql.connector import Error
+import mysql.connector.locales.eng.client_error
+
+
+class TableDb:
+    def __init__(self,_id,_size,_color,_price):
+        self.size = _size
+        self.color = _color
+        self.price = _price
+        self.id =_id
+    def __str__(self):
+        return f"id {self.id} name {self.color} price {self.price} size {self.size}"
 
 class MySQLApp:
     def __init__(self, root):
@@ -16,11 +27,12 @@ class MySQLApp:
             'port':'8889'
         }
         
-        
         self.connection = None
         self.connect_to_db()
         
+        
         self.create_widgets()
+        self.show_records()
         
     def connect_to_db(self):
         try:
@@ -49,6 +61,9 @@ class MySQLApp:
         self.add_button = tk.Button(self.root, text="Добавить запись", command=self.add_record)
         self.add_button.grid(row=3, column=0, padx=5, pady=5)
         
+        self.del_button = tk.Button(self.root, text="удалить ", command=self.del_kbd)
+        self.del_button.grid(row=5, column=1, padx=5, pady=5)
+        
         self.show_button = tk.Button(self.root, text="Показать записи", command=self.show_records)
         self.show_button.grid(row=3, column=1, padx=5, pady=5)
         
@@ -57,10 +72,10 @@ class MySQLApp:
         self.records_listbox.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
         
         # Полоса прокрутки
-        scrollbar = tk.Scrollbar(self.root, orient="vertical")
-        scrollbar.config(command=self.records_listbox.yview)
-        scrollbar.grid(row=4, column=2, sticky="ns")
-        self.records_listbox.config(yscrollcommand=scrollbar.set)
+        self.scrollbar = tk.Scrollbar(self.root, orient="vertical")
+        self.scrollbar.config(command=self.records_listbox.yview)
+        self.scrollbar.grid(row=4, column=2, sticky="ns")
+        self.records_listbox.config(yscrollcommand=self.scrollbar.set)
     
     def add_record(self):
         size = self.size_entry.get()
@@ -88,9 +103,24 @@ class MySQLApp:
             
             for record in records:
                 record_str = f"ID: {record[0]}, Имя: {record[1]}, Возраст: {record[2]}, Email: {record[3]}"
-                self.records_listbox.insert(tk.END, record_str)
+                self.obuv = TableDb(record[0],record[1],record[2],record[3])
+                self.records_listbox.insert(tk.END, self.obuv)
         except Error as e:
             messagebox.showerror("Ошибка", f"Не удалось получить записи: {e}")
+        print(self.obuv)
+        
+    
+    
+    def del_kbd(self):
+        cursor = self.connection.cursor()
+        #del_str ="DELETE FROM 'Gdeto' WHERE 'tipa id' ={'nasha hren'} "
+        self.num = self.records_listbox.curselection()
+        if self.num:
+            self.num = self.num[0]
+        print(f"ВЫБРАЛ ВОТ ЕТО {self.num}")
+        #cursor.execute(del_str,(self.num,))
+       #self.connection.commit()
+        self.show_records()
     
     def clear_entries(self):
         self.size_entry.delete(0, tk.END)
